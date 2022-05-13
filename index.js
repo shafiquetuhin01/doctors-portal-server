f9584d2e04c228e2950cd4aae21eb3262d03593b
Shafique <sfqtuhin04@gmail.com>
2022-05-13 09:36:13 +0600

    updating database

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const express = require('express')
 const app = express()
 require('dotenv').config();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ada80.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        console.log("My data is connected");
+        const servicesCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/service', async(req,res)=>{
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello from doctors portal!')
