220d879185fa89216ccf7a0895ccff8226cd2016
BiasDesk1 <shafiquetuhin01>
2022-05-16 11:10:34 +0600

    server updating doctors-portal

--- index.js
+++ index.js
@@ -17,12 +17,20 @@ async function run(){
         await client.connect();
         console.log("My data is connected");
         const servicesCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('booking');
 
         app.get('/service', async(req,res)=>{
             const query = {};
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.post('/booking', async(req, res)=>{
+          const booking = req.body;
+          const query = {treatment:booking.treatment,date:booking.date,patientEmail:booking.patientEmail}
+          const result = await bookingCollection.insertOne(booking);
+          res.send(result);
         })
     }
     finally{
