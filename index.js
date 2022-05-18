d27612c33384f7291228ab8c4788161315439f37
Shafique <sfqtuhin04@gmail.com>
2022-05-18 20:22:00 +0600

    updating jwt token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,7 +34,8 @@ async function run() {
       const options = {upsert: true};
       const updateDoc = {$set: user};
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      const token = jwt.sign({email: email},process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'})
+      res.send({result,token});
     })
 
     // Warning: This is not the proper way to query multiple collection. 
